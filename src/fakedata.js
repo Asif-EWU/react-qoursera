04933da6017668e7ecf537b6487e4b1c2515edb1
Asif Mohammad <asifratul45@gmail.com>
2020-11-10 23:50:48 +0600

    added fakedata

---
+++ src/fakedata.js
@@ -0,0 +1,198 @@
+const fakedata = [{
+    "course ": {
+      "name": "Transcof",
+      "price": 12.09,
+      "image": "https://robohash.org/similiqueveniamvoluptates.png?size=50x50&set=set1",
+      "date": "01/05/2021"
+    },
+    "university": "Astrahan State Medical Academy",
+    "enrolled": 2430,
+    "instructor ": {
+      "name": "Cristionna Vagg",
+      "image": "https://robohash.org/velitbeataevelit.png?size=50x50&set=set1"
+    }
+  }, {
+    "course ": {
+      "name": "Zontrax",
+      "price": 24.76,
+      "image": "https://robohash.org/aliquamoptioconsectetur.png?size=50x50&set=set1",
+      "date": "14/04/2021"
+    },
+    "university": "Al-Batterjee Medical College",
+    "enrolled": 2921,
+    "instructor ": {
+      "name": "Estrellita McGaffey",
+      "image": "https://robohash.org/quoveroqui.png?size=50x50&set=set1"
+    }
+  }, {
+    "course ": {
+      "name": "Sonair",
+      "price": 10.73,
+      "image": "https://robohash.org/quisexercitationemest.png?size=50x50&set=set1",
+      "date": "08/08/2021"
+    },
+    "university": "Tokyo University of Information Sciences",
+    "enrolled": 4640,
+    "instructor ": {
+      "name": "Paolo Cathie",
+      "image": "https://robohash.org/estsuscipitullam.png?size=50x50&set=set1"
+    }
+  }, {
+    "course ": {
+      "name": "Bytecard",
+      "price": 26.84,
+      "image": "https://robohash.org/estharumillum.png?size=50x50&set=set1",
+      "date": "01/06/2021"
+    },
+    "university": "Université d'Abomey-Calavi (UAC)",
+    "enrolled": 1630,
+    "instructor ": {
+      "name": "Cissy De Malchar",
+      "image": "https://robohash.org/nontemporequi.png?size=50x50&set=set1"
+    }
+  }, {
+    "course ": {
+      "name": "Redhold",
+      "price": 8.49,
+      "image": "https://robohash.org/nisioccaecatiatque.png?size=50x50&set=set1",
+      "date": "07/08/2021"
+    },
+    "university": "Sri Lanka Institute of Advanced Technical Education",
+    "enrolled": 1929,
+    "instructor ": {
+      "name": "Aubrey Desouza",
+      "image": "https://robohash.org/voluptatevoluptatemsapiente.png?size=50x50&set=set1"
+    }
+  }, {
+    "course ": {
+      "name": "Fintone",
+      "price": 20.95,
+      "image": "https://robohash.org/eligendiexnon.png?size=50x50&set=set1",
+      "date": "05/03/2021"
+    },
+    "university": "Universidade Federal do Espírito Santo",
+    "enrolled": 1611,
+    "instructor ": {
+      "name": "Jolene Eisikowitz",
+      "image": "https://robohash.org/nihilfugitqui.png?size=50x50&set=set1"
+    }
+  }, {
+    "course ": {
+      "name": "Bytecard",
+      "price": 15.04,
+      "image": "https://robohash.org/doloreteneturcorporis.png?size=50x50&set=set1",
+      "date": "27/09/2021"
+    },
+    "university": "Houdegbe North American University Benin",
+    "enrolled": 4527,
+    "instructor ": {
+      "name": "Levey Somerbell",
+      "image": "https://robohash.org/molestiasnampossimus.png?size=50x50&set=set1"
+    }
+  }, {
+    "course ": {
+      "name": "Fixflex",
+      "price": 14.68,
+      "image": "https://robohash.org/quiainquaerat.png?size=50x50&set=set1",
+      "date": "26/07/2021"
+    },
+    "university": "Institute of Teachers Education, Sultan Mizan",
+    "enrolled": 3427,
+    "instructor ": {
+      "name": "Trude Tezure",
+      "image": "https://robohash.org/optiovoluptatemaccusamus.png?size=50x50&set=set1"
+    }
+  }, {
+    "course ": {
+      "name": "Span",
+      "price": 12.49,
+      "image": "https://robohash.org/voluptasquised.png?size=50x50&set=set1",
+      "date": "14/05/2021"
+    },
+    "university": "Universitas Sumatera Utara",
+    "enrolled": 1128,
+    "instructor ": {
+      "name": "Cordie Baythrop",
+      "image": "https://robohash.org/natusomnisvoluptas.png?size=50x50&set=set1"
+    }
+  }, {
+    "course ": {
+      "name": "Tres-Zap",
+      "price": 22.29,
+      "image": "https://robohash.org/animiillummolestiae.png?size=50x50&set=set1",
+      "date": "22/03/2021"
+    },
+    "university": "Northern Jiaotong University",
+    "enrolled": 2154,
+    "instructor ": {
+      "name": "Neile Evangelinos",
+      "image": "https://robohash.org/vitaeetiure.png?size=50x50&set=set1"
+    }
+  }, {
+    "course ": {
+      "name": "Cookley",
+      "price": 25.96,
+      "image": "https://robohash.org/repellatassumendanulla.png?size=50x50&set=set1",
+      "date": "24/11/2020"
+    },
+    "university": "Iceland University of Education",
+    "enrolled": 3014,
+    "instructor ": {
+      "name": "Peri Dowbekin",
+      "image": "https://robohash.org/estdoloribussed.png?size=50x50&set=set1"
+    }
+  }, {
+    "course ": {
+      "name": "Span",
+      "price": 5.92,
+      "image": "https://robohash.org/praesentiumrepellatbeatae.png?size=50x50&set=set1",
+      "date": "03/11/2021"
+    },
+    "university": "Universidad Autónoma de Occidente",
+    "enrolled": 1702,
+    "instructor ": {
+      "name": "Waylon Kirkby",
+      "image": "https://robohash.org/illoatconsequatur.png?size=50x50&set=set1"
+    }
+  }, {
+    "course ": {
+      "name": "Tampflex",
+      "price": 5.64,
+      "image": "https://robohash.org/temporevoluptatemnumquam.png?size=50x50&set=set1",
+      "date": "27/09/2021"
+    },
+    "university": "Josai International University",
+    "enrolled": 4731,
+    "instructor ": {
+      "name": "Carling Leverage",
+      "image": "https://robohash.org/porroaccusantiumiste.png?size=50x50&set=set1"
+    }
+  }, {
+    "course ": {
+      "name": "Zoolab",
+      "price": 5.89,
+      "image": "https://robohash.org/absedfacere.png?size=50x50&set=set1",
+      "date": "03/04/2021"
+    },
+    "university": "Ecole Supérieure de Commerce de Reims",
+    "enrolled": 4983,
+    "instructor ": {
+      "name": "Chanda Ferras",
+      "image": "https://robohash.org/quosedunde.png?size=50x50&set=set1"
+    }
+  }, {
+    "course ": {
+      "name": "Transcof",
+      "price": 19.02,
+      "image": "https://robohash.org/nesciuntetdoloribus.png?size=50x50&set=set1",
+      "date": "11/09/2021"
+    },
+    "university": "University of Basrah",
+    "enrolled": 3589,
+    "instructor ": {
+      "name": "Fons Gorgl",
+      "image": "https://robohash.org/quisenimqui.png?size=50x50&set=set1"
+    }
+  }];
+
+  export default fakedata;
